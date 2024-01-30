ee9560d38c96afc1d733e274523cbe10e3aa5db1
Dione Hamilton <dionehamilton784@yahoo.com>
2024-01-30 13:23:19 -0500

    commit

---
+++ server/routes/organization.routes.js
@@ -0,0 +1,20 @@
+//ROUTES FOR ORGANIZATION
+
+const OrganizationController = require('../controllers/organization.controller');
+ 
+module.exports = app => {
+    // READ ROUTES
+    app.get('/api/organizations', OrganizationController.findAllOrganizations);
+    app.get('/api/organizations/:id', OrganizationController.findOneOrganization);
+    app.get('/api/positions', OrganizationController.findAllPositions);
+
+    // UPDATE ROUTE
+    app.patch('/api/organizations/:id', OrganizationController.updateExistingOrganization);
+
+    // CREATE ROUTE
+    app.post('/api/organizations', OrganizationController.createNewOrganization);
+    
+    //DELETE ROUTE
+    app.delete('/api/organizations/:id', OrganizationController.deleteAnExistingOrganization);
+    
+}
