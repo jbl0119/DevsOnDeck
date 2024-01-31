827c805e7172cf6920fb6e17c2b30438ddcd8aae
Jose Bonilla <127805788+jbl0119@users.noreply.github.com>
2024-01-30 21:40:15 -0500

    Test (#9)
    Co-authored-by: Jackson Miller <jackson.d.miller.1@gmail.com>

---
+++ client/src/components/OrgPage.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+// import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const OrgPage = () => {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+
+export default OrgPage;
