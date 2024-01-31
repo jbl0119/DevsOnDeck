5375d81e3663cdc7acd9e5bfee33404b7a72a55a
Jose Bonilla <jabonilla27@gmail.com>
2024-01-31 11:33:54 -0500

    added find all the frameworks to the dev controller.

--- server/controllers/developer.controller.js
+++ server/controllers/developer.controller.js
@@ -101,6 +101,17 @@ module.exports.findAllLanguages = (req, res) => {
 // Modules for Frameworks
 
 module.exports.findAllFramework = (req, res) => {
+    Language.find()
+        .then((allDaFramework) => {
+            res.json({ frameworks: allDaFramework })
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+}
+// Modules for Frameworks
+
+module.exports.findAllFramework = (req, res) => {
     Framework.find()
         .then((allDaFramework) => {
             res.json({ frameworks: allDaFramework })
