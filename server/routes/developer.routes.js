b2b850e01696cb2062712596c8037cee54533cdc
Jose Bonilla <127805788+jbl0119@users.noreply.github.com>
2024-01-30 21:32:02 -0500

    controllers update (#8)
    * controllers update, added login and registration validations in the model and controller.
    
    * added bio to model

--- server/routes/developer.routes.js
+++ server/routes/developer.routes.js
@@ -1,10 +1,12 @@
 const DeveloperController = require('../controllers/developer.controller');
- 
-module.exports = app => {
+
+module.exports = (app) => {
+    console.log('Setting up developer routes');
     app.get('/api/developers', DeveloperController.findAllDevelopers);
     app.get('/api/developers/:id', DeveloperController.findOneSingleDeveloper);
     app.patch('/api/developers/:id', DeveloperController.updateExistingDeveloper);
-    app.post('/api/developers', DeveloperController.createNewDeveloper);
     app.delete('/api/developers/:id', DeveloperController.deleteAnExistingDeveloper);
-    app.get('/api/languages', DeveloperController.findAllLanguages);
+    app.get('/api/languages', DeveloperController.findAllLanguages);    
+    app.post('/api/developers/register', DeveloperController.registerDeveloper);
+    app.post('/api/developers/login', DeveloperController.loginDeveloper);
 }
