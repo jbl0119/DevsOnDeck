7cfcab841d7f6854029bc22f69d6460522b09af8
Jackson Miller <jackson.d.miller.1@gmail.com>
2024-01-30 18:35:05 -0800

    Test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import DevRegister from './components/DevRegister';
+import OrgRegister from './components/OrgRegister';
+import OrgPage from './components/OrgPage.jsx';
 
 function App() {
   
@@ -12,7 +14,8 @@ function App() {
       <Routes>
         
         <Route path="/DevRegister" element={<DevRegister/>}/>
-        
+        <Route path="/orgs/register" element={<OrgRegister/>}/>
+        <Route path="/orgs/dashboard" element={<OrgPage/>}/>
       </Routes>
     </div>
     </BrowserRouter>
