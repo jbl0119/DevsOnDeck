9063bbe76600230bea454b4f0ac49df75ae82f52
Jackson Miller <jackson.d.miller.1@gmail.com>
2024-02-01 12:36:14 -0800

    Registry Changes

--- client/src/components/OrgRegister.jsx
+++ client/src/components/OrgRegister.jsx
@@ -14,7 +14,7 @@ const OrgRegister = () => {
         orgCity: "",
         state: "",
         password: "",
-        confirmPassword: "",
+        confirmPassword: ""
     })
     const navigate = useNavigate();
     const [errors, setErrors] = useState("");
@@ -23,12 +23,42 @@ const OrgRegister = () => {
         setOrganization({...organization, [e.target.name]: e.target.value})
     }
 
-    const onSubmit = (e) => {
+    // const onSubmit = (e) => {
+    //     e.preventDefault()
+    //     axios.post('http://localhost:8000/api/organizations', organization)
+    //         .then((res) => {
+    //             console.log(res)
+    //             navigate('/')
+    //             if(res.data.success) {
+    //                 setOrganization({
+    //                     organizationName: "",
+    //                     firstName: "",
+    //                     lastName: "",
+    //                     contactEmail: "",
+    //                     orgAddress: "",
+    //                     orgCity: "",
+    //                     state: "",
+    //                     password: "",
+    //                     confirmPassword: "",
+    //                 })
+    //                 navigate(`/orgs/dashboard/${res.data.organization._id}`)
+    //             }
+    //             else {
+    //                 setErrors(res.data.error)
+    //             }
+    //             })
+    //         .catch((err) => {
+    //             console.log(err.response.data.error)
+    //             setErrors(err.response.data.error)
+    //         })
+    //     }
+
+        const onSubmit = (e) => {
         e.preventDefault()
-        axios.post('/api/Organization', organization)
+        axios.post('http://localhost:8000/api/organizations', organization)
             .then((res) => {
                 console.log(res)
-                navigate('/')
+                if (res.data.success) {
                 setOrganization({
                     organizationName: "",
                     firstName: "",
@@ -38,15 +68,17 @@ const OrgRegister = () => {
                     orgCity: "",
                     state: "",
                     password: "",
-                    confirmPassword: "",
+                    confirmPassword: ""
                 })
                 navigate(`/orgs/dashboard/${res.data.organization._id}`)
-                })
+            } else {
+                setErrors(res.data.error)
+            }})
             .catch((err) => {
-                console.log(err.response.data.errors)
-                setErrors(err.response.data.errors)
+                console.log(err.response.data.error)
+                setErrors(err.response.data.error)
             })
-        }
+    }
 
 
     return (
@@ -55,42 +87,51 @@ const OrgRegister = () => {
             <div>
             <h1>Organization Sign Up</h1>
             <div>
-                <label for="organizationName" class="form-label">Organization Name</label>
-                <input type="text" class="form-control" name="organizationName" value={organization.organizationName} onChange={onChange}/>
+                <label htmlFor="organizationName" className="form-label">Organization Name</label>
+                <input type="text" className="form-control"  id="organizationName" name="organizationName" value={organization.organizationName} onChange={onChange}/>
+                {errors.organizationName && <p className="text-danger">{errors.organizationName}</p>}
             </div>
             <div>
-                <label for="firstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" name="firstName" value={organization.firstName} onchange={onChange}/>
+                <label htmlFor="firstName" className="form-label">First Name</label>
+                <input type="text" className="form-control"  id="firstName" name="firstName" value={organization.firstName} onChange={onChange}/>
+                {errors.firstName && <p className="text-danger">{errors.firstName}</p>}
             </div>
             <div>
-                <label for="lastName" class="form-label">Last Name</label>
-                <input type="text" class="form-control" name="lastName" value={organization.lastName} onchange={onChange}/>
+                <label htmlFor="lastName" className="form-label">Last Name</label>
+                <input type="text" className="form-control"  id="lastName" name="lastName" value={organization.lastName} onChange={onChange}/>
+                {errors.lastName && <p className="text-danger">{errors.lastName}</p>}
             </div>
             <div>
-                <label for="contactEmail" class="form-label">Contact Email</label>
-                <input type="text" class="form-control" name="contactEmail" value={organization.contactEmail} onchange={onChange}/>
+                <label htmlFor="contactEmail" className="form-label">Email</label>
+                <input type="text" className="form-control" id="contactEmail"  name="contactEmail" value={organization.contactEmail} onChange={onChange}/>
+                {errors.contactEmail && <p className="text-danger">{errors.contactEmail}</p>}
             </div>
             <div>
-                <label for="orgAddress" class="form-label">Org Address</label>
-                <input type="text" class="form-control" name="orgAddress" value={organization.orgAddress} onChange={onChange}/>
+                <label htmlFor="orgAddress" className="form-label">Org Address</label>
+                <input type="text" className="form-control" name="orgAddress" id="orgAddress" value={organization.orgAddress} onChange={onChange}/>
+                {errors.orgAddress && <p className="text-danger">{errors.orgAddress}</p>}
             </div>
             <div>
-                <label for="orgCity" class="form-label">Org City</label>
-                <input type="text" class="form-control" name="orgCity" value={organization.orgCity} onChange={onChange}/>
+                <label htmlFor="orgCity" className="form-label">Org City</label>
+                <input type="text" className="form-control" name="orgCity" id="orgCity" value={organization.orgCity} onChange={onChange}/>
+                {errors.orgCity && <p className="text-danger">{errors.orgCity}</p>}
             </div>
             <div>
-                <label for="state" class="form-label">State</label>
-                <select class="form-control bfh-states" data-country="US" data-state="CA" name="state" value={organization.state} onChange={onChange}>{stateOptions.map((state) => {
+                <label htmlFor="state" className="form-label">State</label>
+                <select className="form-control bfh-states" data-country="US" data-state="CA" name="state" value={organization.state} onChange={onChange}>{stateOptions.map((state) => {
                         return <option key={state} value={state}>{state}</option>
                     })}</select>
+                {errors.state && <p className="text-danger">{errors.state}</p>}
             </div>
             <div>
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" value={organization.password} onChange={onChange}/>
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" name="password" value={organization.password} onChange={onChange}/>
+                {errors.password && <p className="text-danger">{errors.password}</p>}
             </div>
             <div>
-                <label for="confirmPassword" class="form-label">Confirm</label>
-                <input type="password" class="form-control" name="confirmPassword" value={organization.confirmPassword} onchange={onChange}/>
+                <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={organization.confirmPassword} onChange={onChange}/>
+                {errors.confirmPassword && <p className="text-danger">{errors.confirmPassword}</p>}
             </div>
             <div>
                 <input type='submit' value='Register'/>
