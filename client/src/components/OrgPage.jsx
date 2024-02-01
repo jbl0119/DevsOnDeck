22eb1148fbf751833a9b5565eab522bc493bcb20
Jose Bonilla <127805788+jbl0119@users.noreply.github.com>
2024-01-31 21:29:22 -0500

    Frontend changes (#17)
    * Frontend changes
    
    * Route Changes
    
    ---------
    
    Co-authored-by: Jackson Miller <jackson.d.miller.1@gmail.com>

--- client/src/components/OrgPage.jsx
+++ client/src/components/OrgPage.jsx
@@ -1,13 +1,51 @@
-import React, {useState} from 'react';
-// import axios from 'axios';
+import React, {useState, useEffect, useParams} from 'react';
+import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
 
 const OrgPage = () => {
+    const [organization, setOrganization] = useState([]);
+    const [positions, setPositions] = useState([]);
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/organizations/${id}`)
+            .then((res) => {
+                setOrganization(res.data.organization)
+            })
+            .catch((err) => console.log(err))
+    }, [id])
+
+    useEffect(() => {
+        setPositions(organization.Position)
+    }, [organization])
+
 
 
     return (
         <>
+        <div>
+            <div>
+                <div>
+                    <a href="/orgs/add/position">List a New Position</a>
+                </div>
+                <div>
+                    <h2>Positions To Fill</h2>
+                    {
+                        positions.map((pos, idx) => (
+                            <div key={idx}>
+                                <Link to={`/orgs/jobs/${pos._id}`}>{pos.title}</Link>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div>
+                {/* Developer Divs */}
+            </div>
+        </div>
         </>
     )
 }
